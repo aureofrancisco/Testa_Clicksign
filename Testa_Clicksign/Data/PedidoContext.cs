7d0cff9ffa3dfd6cc4576377e391c580e07454c4
aureofrancisco <aureofrancisco@aureofrancisco.com>
2023-04-03 15:10:17 -0300

    Add project files.

---
+++ Testa_Clicksign/Data/PedidoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Testa_Clicksign.Models;
+
+namespace Testa_Clicksign.Data
+{
+    public class PedidoContext : DbContext
+    {
+        public PedidoContext(DbContextOptions<PedidoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+    }
+}
---
+++ Testa_Clicksign/Models/Pedido.cs
@@ -0,0 +1,9 @@
+﻿/*namespace Testa_Clicksign.Models*/
+public class Pedido
+{
+    public int Id { get; set; }
+    public int NumeroPedido { get; set; }
+    public string? TextoDescritivo { get; set; }
+    public DateTime DataPedido { get; set; }
+    public string? StatusPedido { get; set; }
+}
